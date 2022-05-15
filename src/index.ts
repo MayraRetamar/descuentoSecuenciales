61d8336cfd26dbf3a8b433f462f25160ea81df8f
MayraRetamar <104107235+MayraRetamar@users.noreply.github.com>
2022-05-15 14:16:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+let precioProducto: number = 450.5;
+let porcentajeDescuento: number = 0.1;
+let descuento = precioProducto * porcentajeDescuento;
+let precioFinal = precioProducto - descuento;
+
+console.log(precioFinal);
